0e1766c9e5628bcd6a58ef8363c6b681ee7ecc76
Lukas Olson <lukasdragon@live.ca>
2023-06-06 15:43:24 -0700

    Added a way to listen to new messages

---
+++ JavaJotter/PingDemo.cs
@@ -0,0 +1,18 @@
+﻿using SlackNet;
+using SlackNet.Events;
+namespace JavaJotter;
+
+internal class PingDemo : IEventHandler<MessageEvent>
+{
+    private readonly ISlackApiClient _slack;
+    public PingDemo(ISlackApiClient slack)
+    {
+        _slack = slack;
+    }
+
+    public async Task Handle(MessageEvent slackEvent)
+    {
+
+        Console.WriteLine($"Received text from {(await _slack.Users.Info(slackEvent.User)).Name} in the {(await _slack.Conversations.Info(slackEvent.Channel)).Name} channel");
+    }
+}
