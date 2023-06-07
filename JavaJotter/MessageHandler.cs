8e61292e006f54634c5a588ebceea2e2c7b9c55f
Lukas Olson <lukasdragon@live.ca>
2023-06-06 17:02:49 -0700

    More work done

--- JavaJotter/MessageHandler.cs
+++ JavaJotter/MessageHandler.cs
@@ -13,6 +13,8 @@ internal class MessageHandler : IEventHandler<MessageEvent>
     public async Task Handle(MessageEvent slackEvent)
     {
 
-        Console.WriteLine($"Received text from {(await _slack.Users.Info(slackEvent.User)).Name} in the {(await _slack.Conversations.Info(slackEvent.Channel)).Name} channel");
+
+        Console.WriteLine($"Received {slackEvent.Text} from {(await _slack.Users.Info(slackEvent.User)).Name} in the {(await _slack.Conversations.Info(slackEvent.Channel)).Name} channel");
+
     }
 }
